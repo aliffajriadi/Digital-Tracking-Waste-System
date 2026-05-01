928216dada8a630914356ad57b4639afa901c5b1
Naylah013 <naylahamirah13@gmail.com>
2026-05-01 16:26:21 +0700

    add migration for waste tracking table

--- web/database/migrations/0001_01_01_000001_create_users_table.php
+++ web/database/migrations/0001_01_01_000001_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('role_id')->constrained('role');
+            $table->boolean('is_active');
+            $table->dateTime('created_at');
+            $table->string('photo')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -42,8 +42,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
